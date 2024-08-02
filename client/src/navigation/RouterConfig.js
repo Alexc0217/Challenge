e99afe1286c67929b659a00f68e50a06521a65f2
= <alex.moura@walljobs.com.br>
2024-08-01 23:56:52 -0300

    routes

--- client/src/navigation/RouterConfig.js
+++ client/src/navigation/RouterConfig.js
@@ -6,16 +6,21 @@ import CompaniesNew from "../pages/companies/New";
 import CompaniesShow from "../pages/companies/Show";
 
 import EmployeesNew from "../pages/employees/New";
+import Pair from "../pages/chart/Pairs";
+import Subordinates from "../pages/chart/Subordinates";
+import SecondLevel from "../pages/chart/SecondLevel";
 
 export default () => (
   <Routes>
+    <Route path="/" element={<CompaniesIndex />} />
+
     <Route path="/companies" element={<CompaniesIndex />} />
     <Route path="/companies/new" element={<CompaniesNew />} />
     <Route path="/companies/:id" element={<CompaniesShow />} />
 
     <Route path="/employees/new/:company_id" element={<EmployeesNew />} />
-    <Route path="/employees/pairs/:id" />
-    <Route path="/employees/subordinates/:id" />
-    <Route path="/employees/subordinates-second-level/:id" />
+    <Route path="/employees/pairs/:id" element={<Pair />} />
+    <Route path="/employees/subordinates/:id" element={<Subordinates />} />
+    <Route path="/employees/subordinates-second-level/:id" element={<SecondLevel />} />
   </Routes>
 )
